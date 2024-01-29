378beccff87e118f25e9a9359e7c68ac455d4028
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2024-01-29 15:21:40 +0700

    Add some library for node in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), legacy()],
+	plugins: [react(), legacy(), tsconfigPaths(), nodePolyfills()],
 	build: {
 		rollupOptions: {
 			output: {
