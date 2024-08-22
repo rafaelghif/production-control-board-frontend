750791cbef6751195060c2ad99b0ade343011f32
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2024-08-22 10:44:08 +0700

    fix: no production position

--- src/features/dashboard-features/components/NoProduction.tsx
+++ src/features/dashboard-features/components/NoProduction.tsx
@@ -1,6 +1,6 @@
 const NoProduction: React.FC = () => {
 	return (
-		<div className="mt-96 rotate-[-35deg] text-[9.7rem] bg-red-500 p-5 rounded-3xl flex items-center justify-center">
+		<div className="mt-96 text-[9.7rem] bg-red-500 px-6 py-2 rounded-3xl flex items-center justify-center">
 			<span>No Production</span>
 		</div>
 	);
